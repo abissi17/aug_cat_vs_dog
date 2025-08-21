8dcdc9abb050c5b04634c2558a9a238e020699bf
abissi17 <galuxistar@gmail.com>
2025-08-21 15:30:49 -0700

    HW 08/15

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -6,3 +6,8 @@ st.text("Built by Abi")
 uploaded_file = st.file_uploader("Choose as image...", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     st.image(uploaded_file, use_container_width=True)
+
+st.video("https://www.ayclogic.com/wp-content/uploads/2025/07/Crossing-Street.mp4")
+
+def is_cat(f):
+    return f[0].isupper()
