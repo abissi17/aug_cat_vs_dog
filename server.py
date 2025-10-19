355e0076801e0fb32ea312597ed4cc97fea082f8
abissi17 <galuxistar@gmail.com>
2025-10-18 20:06:39 -0700

    Finished Dog & Cat Classifier

--- server.py
+++ server.py
@@ -2,29 +2,35 @@ import streamlit as st
 from fastai.vision.all import *
 
 st.title("Cat vs Dog Classifier")
-st.text("Built by Abi")
+st.text("Built by Abigail Chang")
 
 def is_cat(f):
-    return f[0].isupper()
+    return f[0].isupper() # returns true or false
 
 cat_vs_dog_mod1 = load_learner("cat-vs-dog.pkl")
 
 def predict(image):
     img = PILImage.create(image)
     pred_class, pred_idx, outputs = cat_vs_dog_mod1.predict(img)
-    likehood_is_cat = outputs[1].item()
-    if likehood_is_cat > 0.9:
-        return "Cat"
-    elif likehood_is_cat < 0.1:
-        return "Dog"
+    print("pred_class = "+pred_class) # cat: true, dog: false
+    print("pred_idx = "+str(pred_idx))
+    print("output = "+str(outputs))
+    if pred_class == "True":
+        likehood = outputs[1].item()
+        animal = "cat"
     else:
-        return "Not sure... try another picture!"
+        likehood = outputs[0].item()
+        animal = "dog"
+    if likehood > 0.9:
+        return f"I'm {round(likehood * 100, 2)}% confident this is a {animal.title()}!"
+    else:
+        return f"Not sure... but I'm {round(likehood * 100, 2)}% confident it's a {animal}."
+
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png", "jpeg"])
 if uploaded_file is not None:
     prediction = predict(uploaded_file)
-    st.write(prediction)
+    st.subheader(prediction)
     st.image(uploaded_file,caption="Uploaded Image", use_container_width=True)
-
 
 
